d26834752a8966486c4d6f22e4e607c06665233a
Marco <marco.silva@hub2b.com.br>
2022-03-30 22:00:15 -0300

    added bootstrap view, added basic api connection

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container d-grid gap-4">
+    <div class="card mt-4">
+      <div class="card-body">
+        <form class="row g-3">
+          <div class="col-12">
+            <label for="inputName" class="form-label">Name</label>
+            <input type="text" class="form-control" id="inputName">
+          </div>
+          <div class="col-md-6">
+            <label for="inputSourceSku" class="form-label">Source Sku</label>
+            <input type="text" class="form-control" id="inputSourceSku">
+          </div>
+          <div class="col-md-6">
+            <label for="inputDestionationSku" class="form-label">Destination Sku</label>
+            <input type="text" class="form-control" id="inputDestionationSku">
+          </div>
+          <div class="col-md-6">
+            <label for="inputStock" class="form-label">Stock</label>
+            <input type="text" class="form-control" id="inputStock">
+          </div>
+          <div class="col-md-6">
+            <label for="inputPrice" class="form-label">Price</label>
+            <input type="text" class="form-control" id="inputPrice">
+          </div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Name</th>
+              <th scope="col">Description</th>
+              <th scope="col">Source</th>
+              <th scope="col">Destination</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Price</th>
+              <th class="text-center" scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody v-for="product of productsArray" :key="product.id">
+            <tr>
+              <th scope="row">{{ product.id }}</th>
+              <td>{{ product.name }}</td>
+              <td>{{ product.description }}</td>
+              <td>{{ product.sourceSku }}</td>
+              <td>{{ product.destinationSku }}</td>
+              <td>{{ product.stock }}</td>
+              <td>{{ product.price }}</td>
+              <td class="d-grid gap-2 d-md-flex justify-content-md-center">
+                <button class="btn btn-primary btn-sm" type="button"><i class="bi bi-pencil"></i></button>
+                <button class="btn btn-danger btn-sm me-md-2" type="button"><i class="bi bi-trash"></i></button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Product from './services/products'
+
+  export default {
+
+    data() {
+      return {
+        productsArray: []
+      }
+    },
+
+    mounted() {
+      Product.list().then(res => {
+        this.productsArray = res.data;
+      })
+    }
+
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/services/config.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export const http = axios.create({
+    baseURL: 'https://localhost:7160/api/'
+})
+
